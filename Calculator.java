8ea18feae256d609e88aacbe62432e17153192e3
Hello <ashish151102@gmail.com>
2025-01-07 15:00:22 +0530

    division committed

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ class Calculator{
     System.out.ptintln("Welcome Calculator);
     System.out.ptintln(Add(2,3));
     System.out.println(Sub(4,5));
-
+    System.out.println(div 6,3)
 
    }
   public static int Add(int a,int b){
@@ -11,5 +11,8 @@ class Calculator{
    }
   public static int Sub(int a,int b){
   return a-b;
+ }
+  public static int Sub(int a,int b){
+  return a/b;
 
 }
